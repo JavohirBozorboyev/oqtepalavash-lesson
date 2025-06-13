b525714f7f5c2c3816983133b48f9ec6af6b26b9
JavohirBozorboyev <javohirbozorboyev2002@gmail.com>
2025-06-13 11:13:04 +0500

    ulanish

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./config/db.js";
 import Product from "./routes/Product.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./config/swagger.js";
+import mongoose from "mongoose";
 
 dotenv.config();
-await connectDB();
 
 const app = express();
 app.use(express.json());
@@ -14,4 +13,12 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/products", Product);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server localhost:${PORT} ishga tushdi`));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
+    )
+  )
+  .catch((err) => console.log(err));
