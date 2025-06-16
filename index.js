6e2e47b5699a287265ddb3f90424132d1a614728
JavohirBozorboyev <javohirbozorboyev2002@gmail.com>
2025-06-16 09:51:24 +0500

    cors hatosi

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ import Product from "./routes/Product.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./config/swagger.js";
 import mongoose from "mongoose";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/products", Product);
